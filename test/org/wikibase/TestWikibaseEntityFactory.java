c1f00580bcd420b692f9b623022633d5a2d05a58
Andrei-Cătălin Stroe <acstroe@axway.com>
2019-07-03 11:31:33 +0300

    switch to junit-jupiter

--- test/org/wikibase/TestWikibaseEntityFactory.java
+++ test/org/wikibase/TestWikibaseEntityFactory.java
@@ -1,5 +1,9 @@
 package org.wikibase;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,9 +11,8 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Set;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.wikibase.data.Claim;
 import org.wikibase.data.Entity;
 import org.wikibase.data.Item;
@@ -20,7 +23,7 @@ public class TestWikibaseEntityFactory {
     private Entity q3 = null;
     private Entity q2 = null;
 
-    @Before
+    @BeforeEach
     public void before() throws IOException, WikibaseException {
         q3 = readQFromXMLFile(3);
         q2 = readQFromXMLFile(2);
@@ -47,30 +50,31 @@ public class TestWikibaseEntityFactory {
 
     @Test
     public void testQ3RoLabel() {
-        Assert.assertNotNull(q3);
-        Assert.assertNotNull(q3.getLabels());
-        Assert.assertEquals(q3.getLabels().get("ro"), "viață");
+        assertNotNull(q3);
+        assertNotNull(q3.getLabels());
+        assertEquals(q3.getLabels().get("ro"), "viață");
     }
 
     @Test
     public void testQ2EnSitelink() {
-        Assert.assertNotNull(q2);
-        Assert.assertNotNull(q2.getSitelinks());
-        Assert.assertEquals(q2.getSitelinks().get("enwiki").getPageName(), "Earth");
+        assertNotNull(q2);
+        assertNotNull(q2.getSitelinks());
+        assertEquals(q2.getSitelinks().get("enwiki").getPageName(), "Earth");
     }
 
     @Test
     public void testQ2HighestPointItem() {
-        Assert.assertNotNull(q2);
-        Assert.assertNotNull(q2.getClaims());
+        assertNotNull(q2);
+        assertNotNull(q2.getClaims());
         Set<Claim> p610claims = q2.getClaims().get(new Property("P610"));
-        Assert.assertEquals(1, p610claims.size());
+        assertEquals(1, p610claims.size());
         Claim p610claim = new ArrayList<Claim>(p610claims).get(0);
 
-        Assert.assertNotNull(p610claim.getMainsnak());
-        Assert.assertEquals(p610claim.getMainsnak().getDatatype(), "wikibase-item");
+        assertNotNull(p610claim.getMainsnak());
+        assertEquals(p610claim.getMainsnak().getDatatype(), "wikibase-item");
 
-        Assert.assertTrue(p610claim.getMainsnak().getData() instanceof Item);
-        Assert.assertEquals("513", ((Item) p610claim.getMainsnak().getData()).getEnt().getId());
+        assertTrue(p610claim.getMainsnak().getData() instanceof Item);
+        assertEquals("513", ((Item) p610claim.getMainsnak().getData()).getEnt().getId());
     }
+    
 }
